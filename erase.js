4bbce18d463baf4e1a96cd233362323b4c00f614
Qlec <65467321+Qlec@users.noreply.github.com>
2022-11-27 17:51:01 -0500

    Update erase.js

--- erase.js
+++ erase.js
@@ -1,2 +1,1 @@
-// hi
-c=a=>{for(m=new Uint8Array(3e4),p=i=0,l=[],f=()=>{t=prompt().charCodeAt(),256>t?m[p]=t:f()};i<a.length;a[i++]=r)r=a[i],">"==r&&p++,"<"==r&&p--,"+"==r&&m[p]++,"-"==r&&m[p]--,"."==r&&console.log(String.fromCharCode(m[p])),","==r&&f(),"["==r&&l.push(i),"]"==r?m[p]?i=l[l.length-1]:l.pop():0}
+c=a=>{for(m=new Uint8Array(3e4),p=i=0,l=[];i<a.length;"]"==r?m[p]?i=l[l.length-1]:l.pop():0){if(r=a[i++],">"==r&&p++,"<"==r&&p--,"+"==r&&m[p]++,"-"==r&&m[p]--,"."==r&&console.log(String.fromCharCode(m[p])),","==r)for(;255<(m[p]=prompt().charCodeAt()););"["==r&&l.push(i)}}
